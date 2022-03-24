176ea8b59b3229156ba8d8a4ffa5e39291acbfc4
Fabio Mendes <fabiomendes53@gmail.com>
2022-03-24 20:33:36 -0300

    feat: add backgorund image and basic styles on tasklist screen

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, Text, ImageBackground, StyleSheet, View } from 'react-native'
+
+import todayImage from '../../assets/imgs/today.jpg'
 
 export default class TaskList extends Component {
     render() {
         return (
-            <SafeAreaView>
-                <Text>TaskList</Text>
+            <SafeAreaView style={styles.container}>
+                <ImageBackground source={todayImage}
+                    style={styles.background}>
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <Text>TaskList</Text>
+                </View>
             </SafeAreaView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexGrow: 1
+    },
+    background: {
+        flexGrow: 3
+    },
+    taskList: {
+        flexGrow: 7
+    },
+});
