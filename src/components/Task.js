e4330e3c400efd57e6e6e37a06aa6feb2baa14f2
Fabio Mendes <fabiomendes53@gmail.com>
2022-03-26 15:38:24 -0300

    feat: add format and styles to task date

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,9 +3,15 @@ import { View, Text, StyleSheet } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import commonStyles from '../commonStyles'
 
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
 export default props => {
 
     const doneOrNotStyle = props.doneAt != null ? { textDecorationLine: 'line-through' } : {}
+
+    const date = props.doneAt ? props.doneAt : props.estimateAt
+    const formattedDate = moment(date).locale('pt-br').format('ddd, D [de] MMMM')
     
     return (
         <View style={styles.container}>
@@ -14,7 +20,7 @@ export default props => {
             </View>
             <View>
                 <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
-                <Text>{props.estimateAt + ""}</Text>
+                <Text style={styles.date}>{formattedDate}</Text>
             </View>
         </View>
     )
@@ -67,5 +73,10 @@ const styles = StyleSheet.create({
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.mainText,
         fontSize: 15,
+    },
+    date: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.subText,
+        fontSize: 12,
     },
 })
