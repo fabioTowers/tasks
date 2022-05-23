d6195baefa2986c8714da23459370305276c3a7d
Fabio Mendes <fabiomendes53@gmail.com>
2022-05-23 20:31:53 -0300

    feat: create form validations for signin and signup

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -68,6 +68,18 @@ export default class Auth extends Component {
     }
 
     render() {
+
+        const validations = []
+        validations.push(this.state.email && this.state.email.includes('@'))
+        validations.push(this.state.password && this.state.password.length >= 6)
+        
+        if (this.state.stageNew) {
+            validations.push(this.state.name && this.state.name.trim().length >= 3)
+            validations.push(this.state.password === this.state.confirmPassword)
+        }
+
+        const validForm = validations.reduce((t, a) => t && a)
+
         return (
             <ImageBackground source={backgroundImage} style={styles.background}>
                 <Text style={styles.title}>Tasks</Text>
@@ -109,8 +121,9 @@ export default class Auth extends Component {
                             secureTextEntry={true}
                         />
                     }
-                    <TouchableOpacity onPress={this.signinOrSignup}>
-                        <View style={styles.button}>
+                    <TouchableOpacity onPress={this.signinOrSignup}
+                            disabled={!validForm}>
+                        <View style={[styles.button, validForm ? {} : {backgroundColor: '#AAA'}]}>
                             <Text style={styles.buttonText}>
                                 {this.state.stageNew ? 'Registre-se' : 'Entrar'}
                             </Text>
